97ae5d6b9a4f8e7ad625cfad45688131685be66c
indieg21 <Indiagraham21@gmail.com>
2023-02-13 20:53:20 -0500

    updated code

---
+++ app.js
@@ -0,0 +1,39 @@
+const askButton = document.getElementById("ask-button");
+const questionInput = document.getElementById("question");
+const answerElement = document.getElementById("answer");
+
+const answers = [
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes - definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful."
+];
+
+askButton.addEventListener("click", function() {
+  const question = questionInput.value;
+  if (!question) {
+    alert("Please enter a question.");
+    return;
+  }
+  answerElement.style.display = "block";
+const answer = answers[Math.floor(Math.random() * answers.length)];
+answerElement.textContent = answer;
+eightBallElement.setAttribute("answer", answer);
+
+});
